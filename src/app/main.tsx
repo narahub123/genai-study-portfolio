2d1e8e91469627ed6918b6121d3d5bf8bbf90bea
nara <parknara0213@gmail.com>
2025-06-12 01:49:44 +0900

    [chore] global.css, normalize.css 추가
    Refs: #4

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from ".";
 
+// 중복과 충돌을 피하기 위해서 normalize가 global보다 먼저와야 함
+import "../shared/styles/normalize.css";
+import "../shared/styles/global.css";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
