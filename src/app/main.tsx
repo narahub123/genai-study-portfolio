f6d5d794272edefbefc9098e3047f39606f913fb
nara <parknara0213@gmail.com>
2025-06-12 01:28:16 +0900

    [chore] FSD 기반 폴더 구조 초기 설계 및 적용
    Refs: #3

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./app";
+import { router } from ".";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
---
+++ src/shared/index.ts
@@ -0,0 +1,26 @@
+import {} from "./assets";
+
+import {} from "./constants";
+
+import {} from "./data";
+
+import {} from "./hooks";
+
+import {} from "./layouts";
+
+import {} from "./ui";
+
+import {} from "./utils";
+
+import {} from "./types";
+
+export // assets
+// constants
+// data
+// hooks
+// layouts
+// ui
+// utils
+ {};
+
+export type {};
