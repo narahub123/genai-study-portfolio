636f4142a0f728898aaa20629ab7477273f930c0
nara <parknara0213@gmail.com>
2025-06-12 20:20:25 +0900

    [Feat] 배경 테마 설정 추가
    Refs: #7

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -6,6 +6,8 @@ import { router } from ".";
 // 중복과 충돌을 피하기 위해서 normalize가 global보다 먼저와야 함
 import "../shared/styles/normalize.css";
 import "../shared/styles/global.css";
+import "../shared/styles/variables.css";
+import "../shared/styles/background-theme.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
