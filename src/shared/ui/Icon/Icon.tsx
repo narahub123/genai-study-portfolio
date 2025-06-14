b3745644da89f20fe072034e26e143563d0b5357
nara <parknara0213@gmail.com>
2025-06-14 23:22:47 +0900

    [Feat] forwardRef 적용
    Refs: #10

--- src/shared/ui/Icon/Icon.tsx
+++ src/shared/ui/Icon/Icon.tsx
@@ -12,7 +12,12 @@ import {
   type RoundedToken,
   type SizeToken,
 } from "../../../shared";
-import type { ButtonHTMLAttributes, HTMLAttributes } from "react";
+import {
+  forwardRef,
+  type ButtonHTMLAttributes,
+  type HTMLAttributes,
+  type Ref,
+} from "react";
 
 interface BaseProps {
   className?: string;
@@ -39,60 +44,74 @@ type SpanIconProps = BaseProps &
 type IconProps = ButtonIconProps | SpanIconProps;
 
 // 공통의 요소만 올 수 있음
-const Icon = ({
-  className,
-  onClick,
-  iconName,
-  size = "md",
-  fontSize = "md",
-  rounded = "full",
-  color = "black",
-  title,
-  ...rest
-}: IconProps) => {
-  const classNames = joinClassNames([styles["icon"], tokens[color], className]);
+const Icon = forwardRef<HTMLButtonElement | HTMLSpanElement, IconProps>(
+  (
+    {
+      className,
+      onClick,
+      iconName,
+      size = "md",
+      fontSize = "md",
+      rounded = "full",
+      color = "black",
+      title,
+      ...rest
+    },
+    ref
+  ) => {
+    const classNames = joinClassNames([
+      styles["icon"],
+      tokens[color],
+      className,
+    ]);
 
-  const Inner = icons[iconName];
+    const Inner = icons[iconName];
 
-  if (typeof onClick === "function") {
-    const { disabled = false, ...buttonRest } =
-      rest as ButtonHTMLAttributes<HTMLButtonElement>;
-    return (
-      <button
-        className={classNames}
-        disabled={disabled}
-        onClick={onClick}
-        style={{
-          width: sizeMap[size],
-          height: sizeMap[size],
-          aspectRatio: "1/1",
-          borderRadius: roundedMap[rounded],
-        }}
-        data-title={title}
-        aria-label={title}
-        {...buttonRest}
-      >
-        <Inner aria-hidden="true" style={{ fontSize: fontSizeMap[fontSize] }} />
-      </button>
-    );
-  } else {
-    const { ...spanRest } = rest as HTMLAttributes<HTMLSpanElement>;
-    return (
-      <span
-        className={classNames}
-        aria-hidden="true"
-        style={{
-          width: sizeMap[size],
-          height: sizeMap[size],
-          aspectRatio: "1/1",
-          borderRadius: roundedMap[rounded],
-        }}
-        {...spanRest}
-      >
-        <Inner style={{ fontSize: fontSizeMap[fontSize] }} />
-      </span>
-    );
+    if (typeof onClick === "function") {
+      const { disabled = false, ...buttonRest } =
+        rest as ButtonHTMLAttributes<HTMLButtonElement>;
+      return (
+        <button
+          className={classNames}
+          disabled={disabled}
+          onClick={onClick}
+          style={{
+            width: sizeMap[size],
+            height: sizeMap[size],
+            aspectRatio: "1/1",
+            borderRadius: roundedMap[rounded],
+          }}
+          data-title={title}
+          aria-label={title}
+          ref={ref as Ref<HTMLButtonElement>}
+          {...buttonRest}
+        >
+          <Inner
+            aria-hidden="true"
+            style={{ fontSize: fontSizeMap[fontSize] }}
+          />
+        </button>
+      );
+    } else {
+      const { ...spanRest } = rest as HTMLAttributes<HTMLSpanElement>;
+      return (
+        <span
+          className={classNames}
+          aria-hidden="true"
+          style={{
+            width: sizeMap[size],
+            height: sizeMap[size],
+            aspectRatio: "1/1",
+            borderRadius: roundedMap[rounded],
+          }}
+          ref={ref as Ref<HTMLSpanElement>}
+          {...spanRest}
+        >
+          <Inner style={{ fontSize: fontSizeMap[fontSize] }} />
+        </span>
+      );
+    }
   }
-};
+);
 
 export default Icon;
