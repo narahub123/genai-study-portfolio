0fc56900be2104e8c8e8f27ed2db51ea0ee60a82
nara <parknara0213@gmail.com>
2025-06-15 10:48:04 +0900

    [Feat] card 기본 레이아웃 설정
    Refs: #12

--- src/features/report/pages/ReportNavigation/ReportNavigation.tsx
+++ src/features/report/pages/ReportNavigation/ReportNavigation.tsx
@@ -11,9 +11,26 @@ const ReportNavigation = ({ className }: ReportNavigationProps) => {
 
   return (
     <div className={classNames}>
-      <Link to="von-neumman-architecture">폰 노이만 구조</Link>
-      <Link to="semantic-tages-accessibility-dom">
-        시맨틱 태그, 접근성 그리고 DOM
+      <Link to="von-neumman-architecture" className={styles["card"]}>
+        <img
+          src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbupjTX%2FbtrgnsgHKf9%2FAFF5V4IRsX8qE7c4Tzj7J1%2Fimg.png"
+          alt="폰 노이만 구조"
+          className={styles["thumbnail"]}
+        />
+        <div className={styles["wrapper"]}>
+          <p>폰 노이만 구조</p>
+        </div>
+      </Link>
+      <Link to="semantic-tages-accessibility-dom" className={styles["card"]}>
+        <img
+          src="https://cheonmro.github.io/images/dom-tree.png"
+          alt="시맨틱 태그, 접근성 그리고 DOM"
+          className={styles["thumbnail"]}
+        />
+        <div className={styles["wrapper"]}>
+          <p>시맨틱 태그, 접근성 그리고 DOM</p>
+          <p>2025. 06. 15</p>
+        </div>
       </Link>
     </div>
   );
