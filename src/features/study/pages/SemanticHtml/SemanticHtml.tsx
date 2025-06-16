4481f368fe569f2d89795132bacaa40813a209bb
nara <parknara0213@gmail.com>
2025-06-16 17:46:34 +0900

    [Fix] SemanticHtml 수정
    Refs: #15

--- src/features/study/pages/SemanticHtml/SemanticHtml.tsx
+++ src/features/study/pages/SemanticHtml/SemanticHtml.tsx
@@ -8,7 +8,11 @@ interface SemanticHtmlProps {
 const SemanticHtml = ({ className }: SemanticHtmlProps) => {
   const classNames = joinClassNames([styles["semantic__html"], className]);
 
-  return <div className={classNames}>SemanticHtml</div>;
+  return (
+    <div className={classNames}>
+      <h1 className={styles["h1"]}>시맨틱 HTML</h1>
+    </div>
+  );
 };
 
 export default SemanticHtml;
